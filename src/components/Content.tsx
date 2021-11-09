6957547a3a519ef4a651132a85a6cdbc7acc0918
Pablo Alan de Freitas Cardoso <pabloxt14@gmail.com>
2021-11-09 14:13:50 -0300

    Finished Aplication's Componentization

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,39 @@
-export function Content() {
+import { MovieCard } from './MovieCard';
+import { ContentHeader } from './ContentHeader';
+
+interface ContentProps {
+  selectedGenre: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  };
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+}
+
+
+export function Content(props: ContentProps) {
   // Complete aqui
+  return (
+    <div className="container">
+      {/* ContentHeader Component */}
+      <ContentHeader title={props.selectedGenre.title} />
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
---
+++ src/components/ContentHeader.tsx
@@ -0,0 +1,12 @@
+
+interface ContentHeaderProps {
+    title: string;
+}
+
+export function ContentHeader(props: ContentHeaderProps) {
+    return (
+        <header>
+            <span className="category">Categoria:<span> {props.title}</span></span>
+        </header>
+    );
+}
